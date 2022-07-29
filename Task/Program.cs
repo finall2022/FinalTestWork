a37c2cb63d4110527898db13b0f7d808250a35c5
finall2022 <finall.git2022@gmail.com>
2022-07-29 14:26:11 +0300

    дополнено решение задачи

--- Task/Program.cs
+++ Task/Program.cs
@@ -10,10 +10,31 @@
 
 ["Russia", "Denmark", "Kazan"] -> [] */
 
+//Функция заполнения массива требуемыми данными
+void FillNewArray(string[] tempArray, string[] oldArray)
+{
+    int i = 0;
+    int j = 0;
+    {
+        for (i = 0; i < oldArray.Length; i++)
+        {
+            string s = oldArray[i];
+            if (s.Length <= 3)
+            {
+                tempArray[j] = oldArray[i];
+                Console.Write($"{tempArray[j]} ");
+                j++;
+            }
+        }
+    }
+}
+
+//Начало программы
+Console.WriteLine("Программа, которая из имеющегося массива строк формирует массив из строк,длина которых меньше либо равна 3 символа");
 Console.Write("Введите размер массива: ");
 string inputNumberOfArray = Console.ReadLine();
 int numberOfArray = int.Parse(inputNumberOfArray);
-Console.WriteLine($"Размер массива: {numberOfArray}");
+//Инициализация и заполнение массива
 string[] arrayOfString = new string[numberOfArray];
 int i = 0;
 int count = 0;
@@ -22,24 +43,15 @@ for (i = 0; i < numberOfArray; i++)
     Console.WriteLine($"Введите {i + 1}-й элемент массива: ");
     arrayOfString[i] = Console.ReadLine();
     string s = arrayOfString[i];
-    Console.WriteLine("Символов в строке: {0}", s.Length);
     if (s.Length <= 3)
     {
         count++;
     }
 }
-
-Console.WriteLine($"Новый массив из {count} элементов ");
+//Заполнение нового массива нужными значениями
+Console.WriteLine($"Новый массив будет состоять из {count} элементов ");
 string[] newArray = new string[count];
-int j = 0;
-Console.WriteLine("Массив, в котором содержатся элеменды, длина которых меньше или равна 3-м символам: ");
-for (i = 0; i < arrayOfString.Length; i++)
-{
-    string s = arrayOfString[i];
-    if (s.Length <= 3)
-    {
-        newArray[j] = arrayOfString[i];
-        Console.Write($"{newArray[j]} ");
-        j++;
-    }
-}
+Console.WriteLine(
+    "Массив, в котором содержатся элеменnы, длина которых меньше или равна 3-м символам: "
+);
+FillNewArray(newArray, arrayOfString);
